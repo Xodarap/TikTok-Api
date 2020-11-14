e60325ce4f50b03e300345eddca7367bc718d314
Ben West <Xodarap@users.noreply.github.com>
2020-11-13 16:59:50 -0800

    broken again

--- ben.py
+++ ben.py
@@ -1,4 +1,4 @@
 from TikTokApi import TikTokApi
 api = TikTokApi()
 # api.byUsername('therock', custom_verifyFp='verify_khgy7u5g_41BRrq9U_AjGW_49J3_8Y0m_Ubbo8FpbweS7', did='6894769024258442757', count=30)
-api.getUserObject('therock', custom_verifyFp='verify_khgy7u5g_41BRrq9U_AjGW_49J3_8Y0m_Ubbo8FpbweS7', did = '559798762', count=30)
+api.getUserObject('therock', custom_verifyFp='verify_khgyofu6_YP8cEg4o_QpRA_4lbj_89kv_LOaIzzExQcAs', did = '6894770323305154053', count=30)

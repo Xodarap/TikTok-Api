9fdff34c601ef827ea0e4ed58df7e22cd2762d30
Ben West <Xodarap@users.noreply.github.com>
2020-11-13 17:18:09 -0800

    working

--- ben.py
+++ ben.py
@@ -1,9 +1,9 @@
 from TikTokApi import TikTokApi
 api = TikTokApi()
-# api.byUsername('therock', custom_verifyFp='verify_khgyofu6_YP8cEg4o_QpRA_4lbj_89kv_LOaIzzExQcAs', did='6894770323305154053', count=30,
+api.byUsername('therock', custom_verifyFp='verify_khgyofu6_YP8cEg4o_QpRA_4lbj_89kv_LOaIzzExQcAs', did='6894770323305154053', count=30,
 
-#                 browser_language='en-US',
-#                 browser_platform= 'Win32',
-#                 browser_name= 'Mozilla')
-x = api.getUserObject('therock', custom_verifyFp='verify_khgyofu6_YP8cEg4o_QpRA_4lbj_89kv_LOaIzzExQcAs', did = '6894770323305154053', count=30)
-print(x)
+                browser_language='en-US',
+                browser_platform= 'Win32',
+                browser_name= 'Mozilla')
+# x = api.getUserObject('therock', custom_verifyFp='verify_khgyofu6_YP8cEg4o_QpRA_4lbj_89kv_LOaIzzExQcAs', did = '6894770323305154053', count=30)
+# print(x)
